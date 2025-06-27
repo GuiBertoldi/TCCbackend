3063d4233c035d443afa4c492b1e33556af77fd7
GuiBertoldi <guilherme_chuia@hotmail.com>
2025-06-27 00:31:01 -0300

    Ajustando url para o front

--- src/main/java/com/tcc/backend/config/CorsConfig.java
+++ src/main/java/com/tcc/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins("https://main.d3lf84y1jrpujb.amplifyapp.com", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept")
                 .allowCredentials(true);
